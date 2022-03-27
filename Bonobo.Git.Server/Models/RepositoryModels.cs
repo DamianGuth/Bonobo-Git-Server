080f747a9eb3eba0a5de563a4649f9d6b1b0c1ca
Damian Guth <guth-d@web.de>
2022-03-27 18:58:58 +0200

    Add date formatting.

--- Bonobo.Git.Server/Models/RepositoryModels.cs
+++ Bonobo.Git.Server/Models/RepositoryModels.cs
@@ -174,7 +174,7 @@ namespace Bonobo.Git.Server.Models
 
         [Display(ResourceType = typeof(Resources), Name = "Repository_Tree_CommitDate")]
         public DateTime? CommitDate { get; set; }
-        public string CommitDateString { get { return CommitDate.HasValue ? CommitDate.Value.ToString() : CommitDate.ToString(); } }
+        public string CommitDateString { get { return CommitDate.HasValue ? CommitDate.Value.ToString("dd.MM.yyyy - HH:mm:ss") : CommitDate?.ToString("dd.MM.yyyy - HH:mm:ss"); } }
 
         [Display(ResourceType = typeof(Resources), Name = "Repository_Tree_Author")]
         public string Author { get; set; }
